5104f4899014701e8f9b35dff9315c2b98041289
RokueL <kazix135@gmail.com>
2023-09-18 09:44:58 +0900

    Update mainCotroller.cs

--- Assets/Resources/Script/Object/mainCotroller.cs
+++ Assets/Resources/Script/Object/mainCotroller.cs
@@ -1,10 +1,16 @@
 using SceneType;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mainCotroller : MonoBehaviour
 {
+    TextMeshPro KillScore;
+    TextMeshPro DistanceScore;
+    TextMeshPro coinScore;
+
 
     // Start is called before the first frame update
     void Start()
